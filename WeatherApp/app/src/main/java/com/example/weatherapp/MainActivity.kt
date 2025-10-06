606341a7e535f9f59bbd72fd34f002c05643d915
niyi-16 <dasil.adam@gmail.com>
2025-10-06 02:06:03 -0300

    Added top and bottom bars

--- WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -9,13 +9,32 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.weatherapp.ui.screen.CurrentWeather
+import com.example.weatherapp.ui.screen.DailyForecast
 import com.example.weatherapp.ui.theme.WeatherAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,14 +43,94 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             WeatherAppTheme {
-                    Greeting(
-                        name = "Android"
-                    )
-                }
+                DisplayUI()
             }
         }
     }
+}
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DisplayUI() {
+    val navController = rememberNavController()
+
+    var selectedItem by remember { mutableIntStateOf(0) }
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.inverseSurface
+                ),
+                title = { Text("Weather App") }
+            )
+        }, //End of topBar
+
+        bottomBar = {
+            NavigationBar(
+                containerColor = MaterialTheme.colorScheme.primaryContainer,
+                contentColor = MaterialTheme.colorScheme.primary
+            )
+            // Navigation items go here
+            {
+                //Link to Now screen
+                NavigationBarItem(
+                    label = { Text("Current Weather") },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.cloud_bolt_svgrepo_com),
+                            modifier = Modifier.size(30.dp),
+                            contentDescription = "Current Weather"
+                        )}
+                    ,
+                    selected = selectedItem == 0,
+                    onClick = {
+                        selectedItem = 0
+                        navController.navigate("CurrentWeather")
+                    }
+                    ) //End of bottomBar.NavigationBar.NavigationBarItem
+
+                //Link to Forcast Screen
+                NavigationBarItem(
+                    label = { Text("Forecast") },
+                    icon = {
+                        Icon(
+                            painter = painterResource(R.drawable.cloud_sun_alt_svgrepo_com),
+                            modifier = Modifier.size(30.dp),
+                            contentDescription = "Forecast"
+                        )}
+                    ,
+                    selected = selectedItem == 1,
+                    onClick = {
+                        selectedItem = 1
+                        navController.navigate("DailyForecast")
+                    }
+                    ) //End of bottomBar.NavigationBar.NavigationBarItem
+            } //End of bottomBar.NavigationBar
+
+        } // End of bottomBar
+    )
+
+    { innerPadding ->
+        // Routing
+        NavHost(
+            navController =navController,
+            startDestination = "CurrentWeather",
+            modifier = Modifier.padding(innerPadding)
+        ){
+            // Displays Current Weather
+            composable (route = "CurrentWeather"){
+                CurrentWeather()
+            }
+
+            composable(route = "DailyForecast") {
+                DailyForecast()
+            }
+
+        }
+    }
+}
 
 @Composable
 fun Greeting(name: String) {
@@ -42,7 +141,7 @@ fun Greeting(name: String) {
             .background(color = Color.LightGray),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
-        ) {
+    ) {
         Text(
             text = "Hello $name!"
         )
