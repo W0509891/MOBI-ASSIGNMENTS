b020916b844ed2ea80d5f9c5f2d74c8da2b3502b
w0509891 <w0509891@nscc.ca>
2025-10-09 17:11:16 -0300

    Made Classes to spec.

--- WeatherApp/app/src/main/java/com/example/weatherapp/models/Weather.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/models/Weather.kt
@@ -1,8 +1,50 @@
 package com.example.weatherapp.models
 
-class Weather (
-    val weatherCondition: String,
-    val tempretureHigh: Int,
-    val tempretureLow: Int,
-    val weatherDescriptor: String
+
+// ========================Main Classes======================== //
+data class Weather (
+    val current: Current,
+    val forecast: List<Forecast>,
+
     )
+
+
+data class Current(
+    val weatherImage: String, //Ask - What should be data type
+    val condition: String,
+    val temperature: Temperature,
+    val precipitation: Precipitation,
+    val wind: Wind
+
+)
+
+
+data class Forecast(
+    val date: String, //Ask - What should be data type
+    val weatherImage: String, //Ask - What should be data type
+    val temperature: Temperature,
+    val condition: String,
+    val precipitation: Precipitation,
+    val wind: Wind,
+    val humidity: Double
+
+)
+
+// ========================Helper Classes======================== //
+
+data class Temperature(
+    val actual: Int,
+    val high: Int,
+    val low: Int
+)
+
+data class Precipitation(
+    val type: String,
+    val amount: Int,
+    val probability: Int
+)
+
+data class Wind(
+    val direction: String,
+    val speed: Int
+)
