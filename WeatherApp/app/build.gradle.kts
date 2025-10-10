52bed276294c9eae141dae429409113adbfef830
w0509891 <w0509891@nscc.ca>
2025-10-10 00:54:36 -0300

    Added async Dependency

--- WeatherApp/app/build.gradle.kts
+++ WeatherApp/app/build.gradle.kts
@@ -49,7 +49,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    // personally added
     implementation("androidx.navigation:navigation-compose:2.9.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
