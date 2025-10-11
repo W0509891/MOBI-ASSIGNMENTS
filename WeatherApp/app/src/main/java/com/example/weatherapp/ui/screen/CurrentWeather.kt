402f97becb5d893039e35d318aa97fe27138d6e3
w0509891 <w0509891@nscc.ca>
2025-10-10 23:51:35 -0300

    Formatted properly, modified layout, code clean up and comments

--- WeatherApp/app/src/main/java/com/example/weatherapp/ui/screen/CurrentWeather.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/ui/screen/CurrentWeather.kt
@@ -1,86 +1,84 @@
 package com.example.weatherapp.ui.screen
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.weatherapp.R
+import com.example.weatherapp.models.Current
 
 @Composable
-fun CurrentWeather() {
+fun CurrentWeather(current: Current?) {
+
     // Main Container
     Column(
         modifier = Modifier
-            .fillMaxSize()
-            .background(color = Color.LightGray)
-        ,
-        verticalArrangement = Arrangement.Center,
+            .fillMaxSize(),
+        verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally,
-    ){
+    ) {
+        Box(
+            contentAlignment = Alignment.Center
+        ) {
+            Image(
+                painter = painterResource(current?.weatherImage!!.weatherBackgroundRId),
+                contentDescription = ""
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            )
+            {
+                WeatherCondition(current)
+            }
 
-        Row (
-            modifier = Modifier
-                .fillMaxWidth()
-//                .offset(y= (-300).dp)
-                .background(color = Color(0xFFE3EDEF))
-            ,
-//            horizontalArrangement = Arrangement.Center
-        ){
-            WeatherCondition()
         }
     }
 }
 
 @Composable
-fun WeatherCondition() {
+fun WeatherCondition(cw: Current?) {
 
-        Column(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                //outline for visual understanding
-//                .background(color = Color(0xFFff4500))
-            ,
-            verticalArrangement = Arrangement.SpaceEvenly,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ){
-            // Image changes depending on th weather
-            Image(
-                modifier = Modifier.size(80.dp),
-                painter = painterResource(R.drawable.cloud_sun_svgrepo_com),
-                contentDescription = ""
-            )
-            Text("Overcast")                                   // Weather Conditon
-            Text("6" + "°" + "C", fontSize = 25.sp)            // Actual tempreture
-            Text("Feels like" + " 7°C", fontSize = 10.sp)      // wind tempreture
-            Text("Wind " + "SW " + "18kph", fontSize = 10.sp)  // wind speeds
-        }
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.SpaceBetween,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        // Image changes depending on the weather
+        Image(
+            modifier = Modifier.size(150.dp), //Todo: Edit transparent space around image
+            painter = painterResource(cw?.weatherImage!!.weatherIconRId),
+            contentDescription = "Weather Condition"
+        )
+        Text(text = cw.condition, fontSize = 25.sp)  // Weather Condition
 
+        Text(
+            "${cw.temperature.actual}°C", // Actual temperature
+            fontSize = 35.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Text(
+            "Feels like " + "${cw.temperature.actual}°C", // wind temperature
+            fontSize = 16.sp
+        )
+
+        Text(
+            "Wind " + "${cw.wind.direction} ${cw.wind.speed} kph", // wind speeds
+            fontSize = 16.sp
+        )
+    }
 }
-@Preview(showBackground = true)
-@Composable
-fun CurrentWeatherPreview() {
-    CurrentWeather()
-}
-
-
-
-
-
-
 
