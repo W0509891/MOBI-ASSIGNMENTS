99b729d5de635f18d79c2837de751c4264f7e5b6
W0509891 <W0509891@nscc.ca>
2025-10-21 15:29:39 -0300

    Modified api call

--- WeatherApp/app/src/main/java/com/example/weatherapp/services/WeatherService.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/services/WeatherService.kt
@@ -2,6 +2,7 @@ package com.example.weatherapp.services
 
 import com.example.weatherapp.models.Current
 import com.example.weatherapp.models.Forecast
+import com.example.weatherapp.models.Weather
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -12,5 +13,5 @@ interface WeatherService {
     suspend fun getCurrent(): Current
 
     @GET("forecast.json?key=88585df785c74f2a902134735251610&q=Halifax&days=7&aqi=no&alerts=no")
-    suspend fun getForecast(): Forecast
+    suspend fun getWeather(): Weather
 }
