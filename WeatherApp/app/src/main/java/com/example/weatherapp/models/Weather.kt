8191bbc38af4a6869b26b91c5f440acc7f1ee9c0
W0509891 <W0509891@nscc.ca>
2025-10-21 15:26:04 -0300

    REstuctured Properly to fit api

--- WeatherApp/app/src/main/java/com/example/weatherapp/models/Weather.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/models/Weather.kt
@@ -5,31 +5,24 @@ import com.google.gson.annotations.SerializedName
 // ========================Main Classes======================== //
 data class Weather(
     val current: Current,
-    val forecast: List<Forecast>,
+    val forecast: Forecastday,
 )
 
 
 data class Current(
-    val temp: Double,
     val condition: Condition,
+    @SerializedName("temp_c") val temp: Double,
     @SerializedName("wind_dir") val windDirection: String,
     @SerializedName("wind_kph") val windSpeed: Double,
     @SerializedName("windchill_c") val windTemp: Double,
-    @SerializedName("feelslike_c") val feels: Int,
+    @SerializedName("feelslike_c") val feels: Double,
 )
 
 
 data class Forecast(
     var date: String, //Ask - What should be data type
-    val condition: Condition,
-    @SerializedName("maxtemp_c") val tempHigh: Double,
-    @SerializedName("mintemp_c") val tempLow: Double,
-    @SerializedName("daily_chance_of_rain") val preciChance: Int,
-    @SerializedName("totalprecip_mm") val preciAmount: Double,
-    @SerializedName("wind_kph") val windSpeed: Double,
-    @SerializedName("avghumidity") val humidity: Double,
-
-    )
+    val day: Day
+)
 
 // ========================Helper Classes======================== //
 
@@ -37,4 +30,19 @@ data class Forecast(
 data class Condition(
     val text: String,
     val icon: String,
+)
+
+data class Day(
+    val condition: Condition,
+    @SerializedName("maxtemp_c") val tempHigh: Double,
+    @SerializedName("mintemp_c") val tempLow: Double,
+    @SerializedName("daily_chance_of_rain") val preciChance: Int,
+    @SerializedName("totalprecip_mm") val preciAmount: Double,
+    @SerializedName("maxwind_kph") val windSpeed: Double,
+    @SerializedName("avghumidity") val humidity: Double,
+)
+
+
+data class Forecastday(
+    @SerializedName("forecastday") val forecast: List<Forecast>
 )
