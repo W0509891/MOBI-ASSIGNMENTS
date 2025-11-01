fd8499015bc6f933e6c2cd6cfa2e930f8e7f409e
w0509891 <w0509891@nscc.ca>
2025-10-31 21:18:33 -0300

    Added Location permissions and packages

--- WeatherApp/app/build.gradle.kts
+++ WeatherApp/app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     implementation("com.squareup.retrofit2:retrofit:3.0.0")
     // Retrofit GSON Converter
     implementation("com.squareup.retrofit2:converter-gson:3.0.0")
+
+    // Google Play location service
+    implementation ("com.google.android.gms:play-services-location:21.0.1")
+// Accompanist permissions - makes getting permissions easier
+    implementation ("com.google.accompanist:accompanist-permissions:0.34.0")
 }
